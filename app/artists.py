aab09e537b75f9f478fa7fe98d035c1056100b28
gramsa23 <89297908+gramsa23@users.noreply.github.com>
2022-01-29 16:15:33 +0300

    updated

--- app/artists.py
+++ app/artists.py
@@ -7,11 +7,13 @@ class Artists(Spotify):
         super().__init__(CLIENT_ID, CLIENT_SECRET, BASE_URL)
 
     def get_artist(self, id=None):
+        
         artist_data = requests.get(self.base_url + "artists/" + id, headers=self.headers).json()
 
         return artist_data
 
     def get_artists(self, ids=None):
+        
         artists_data = requests.get(self.base_url + "artists?ids=" + ids, headers=self.headers).json()
 
         return artists_data
@@ -28,6 +30,7 @@ class Artists(Spotify):
         return artists_top_tracks
 
     def get_related_artists(self, id=None):
+        
         related_artists = requests.get(self.base_url + "artists/" + id + "/related-artists", headers=self.headers).json()
 
         return related_artists
--- app/test.py
+++ app/test.py
@@ -5,18 +5,71 @@ from artists import Artists
 from tracks import Tracks
 import pandas as pd
 
-gramsa = Artists()
+gramsa = Artists(CLIENT_ID=None, CLIENT_SECRET=None)
 
-def top_tracks_table(id=None, market=None):
-    top_tracks = gramsa.get_artists_top_tracks(id=id, market=market)
+def artist_table(id=None):
 
-    top_tracks_list = []
+    artist_data = gramsa.get_artist(id=id)
 
-    for i in range(10):
-        top_tracks_list.append(top_tracks["tracks"][i]["name"])
-
-    df = pd.DataFrame(data=top_tracks_list, columns=["Tracks"])
+    df = pd.DataFrame({
+        "Name": [artist_data["name"]],
+        "Popularity": [artist_data["popularity"]],
+        "Followers": [artist_data["followers"]["total"]]
+    })
 
     return df
 
+def artists_table(ids=None):
 
+    artists_data = gramsa.get_artists(ids=ids)
+
+    names = []
+    
+    for i in range(len(ids.split(","))):
+        names.append(artists_data["artists"][i]["name"])
+
+    df = pd.DataFrame({"Name": names})
+
+    return df
+
+def top_tracks_table(id=None, market=None):
+    
+    top_tracks = gramsa.get_artists_top_tracks(id=id, market=market)
+
+    top_tracks_list = []
+    artist_name = []
+    duration_ms = []
+    album = []
+    release_date = []
+
+    for i in range(10):
+        top_tracks_list.append(top_tracks["tracks"][i]["name"])
+        artist_name.append(top_tracks["tracks"][i]["popularity"])
+        duration_ms.append(top_tracks["tracks"][i]["duration_ms"])
+        album.append(top_tracks["tracks"][i]["album"]["name"])
+        release_date.append(top_tracks["tracks"][i]["album"]["release_date"])
+
+    df = pd.DataFrame({
+        "Track": top_tracks_list,
+        "Album": album,
+        "Release Date": release_date,
+        "Popularity": artist_name, 
+        "Duration_ms": duration_ms
+    })
+
+    return df
+
+def related_artists_table(id=None):
+
+    related_artists = gramsa.get_related_artists(id=id)
+
+    names = []
+
+    for i in range(20):
+        names.append(related_artists["artists"][i]["name"])
+
+    df = pd.DataFrame({"Name": names})
+
+    return df
+
+print(related_artists_table(id="5c3GLXai8YOMid29ZEuR9y"))
