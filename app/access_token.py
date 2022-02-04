a33a982abb4909b7c9b90f6855a7c364b09e6b7c
gramsa23 <89297908+gramsa23@users.noreply.github.com>
2022-02-04 20:15:04 +0300

    ignoretest

--- app/access_token.py
+++ app/access_token.py
@@ -3,7 +3,7 @@ import requests
 
 class Spotify():
     
-    CLIENT_ID = "ignore"
+    CLIENT_ID = "ignoretest"
     CLIENT_SECRET = ""
     BASE_URL = "https://api.spotify.com/v1/"
     AUTH_URL="https://accounts.spotify.com/api/token"
